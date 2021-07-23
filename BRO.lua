455f0d7c0b1429708250c480b1519019d043f784
SRC-BRO <87868861+SRC-BRO@users.noreply.github.com>
2021-07-23 18:45:11 +0300

    Update BRO.lua

--- BRO.lua
+++ BRO.lua
@@ -164,7 +164,7 @@ io.popen("mkdir File_Bot")
 io.popen("cd File_Bot && rm -rf commands.lua.1") 
 io.popen("cd File_Bot && rm -rf commands.lua.2") 
 io.popen("cd File_Bot && rm -rf commands.lua.3") 
-io.popen("cd File_Bot && wget https://raw.githubusercontent.com/SRC-BRO/Files_BRO/main/File_Bot/commands.lua") 
+io.popen("cd File_Bot && wget https://raw.githubusercontent.com/SRC-BRO/Files_BRO/main/Files_BRO/commands.lua") 
 t = "\27[35m".."\nAll Files Started : \n____________________\n"..'\27[m'
 i = 0
 for v in io.popen('ls File_Bot'):lines() do
@@ -3906,7 +3906,7 @@ t = " ߷︙الملف » "..file.."\n ߷︙تم تعطيل ملف \n"
 else
 t = " ߷︙بالتاكيد تم تعطيل ملف → "..file.."\n"
 end
-local json_file, res = https.request("https://raw.githubusercontent.com/SRC-BRO/Files_BRO/main/File_Bot/"..file)
+local json_file, res = https.request("https://raw.githubusercontent.com/SRC-BRO/Files_BRO/main/Files_BRO/"..file)
 if res == 200 then
 os.execute("rm -fr File_Bot/"..file)
 send(msg.chat_id_, msg.id_,t) 
@@ -3926,7 +3926,7 @@ t = " ߷︙بالتاكيد تم تفعيل ملف → "..file.." \n"
 else
 t = " ߷︙الملف » "..file.."\n ߷︙تم تفعيل ملف \n"
 end
-local json_file, res = https.request("https://raw.githubusercontent.com/SRC-BRO/Files_BRO/main/File_Bot/"..file)
+local json_file, res = https.request("https://raw.githubusercontent.com/SRC-BRO/Files_BRO/main/Files_BRO/"..file)
 if res == 200 then
 local chek = io.open("File_Bot/"..file,'w+')
 chek:write(json_file)
